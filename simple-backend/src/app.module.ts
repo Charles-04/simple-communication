343d7495db9f44c7feb3f63a7b7e25c90e7cf26e
Charles <charlesmir04@gmail.com>
2022-07-02 16:13:42 +0100

    worked on file-upload

--- simple-backend/src/app.module.ts
+++ simple-backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { DepartmentController } from './department/department.controller';
 import { FeesController } from './fees/fees.controller';
+import { FileUploadModule } from './file-upload/file-upload.module';
 
 @Module({
-  imports: [],
+  imports: [FileUploadModule],
   controllers: [AppController, UserController, DepartmentController, FeesController],
   providers: [AppService],
 })
